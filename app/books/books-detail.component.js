c5d002d91d165174e81b7f4eb57eac979054e0ce
Dariel Barroso <dariel_games@yahoo.com>
2018-07-09 22:21:33 -0400

    Removed extra files Fixed review drop down, not has a default value Page should no longer reload when submitting a review

--- app/books/books-detail.component.js
+++ app/books/books-detail.component.js
@@ -7,50 +7,49 @@ const BooksDetailComponent = {
         $ctrl.book = {};
         $ctrl.comments = {};
         $ctrl.rating = 0;
+        $ctrl.isAnon = 0;
+        $ctrl.ratings = [0, 1, 2, 3, 4, 5];
         let html = angular.element('html');
 
-        //TODO Wrap in a function, then call from other places to refresh
-        api.getBookDetail(bookId).then(function (response) {
-            $ctrl.book = response.data;
-        }, function () {
-            //404 not found
-        });
+        function getData() {
+            api.getBookDetail(bookId).then(function (response) {
+                $ctrl.book = response.data;
+            }, function () {
+                //404 not found
+            });
 
 
-        //Get books list from web api
-        api.getComments(bookId).then(function (response) {
-            $ctrl.comments = response.data;
-        }, function () {
-            //404 not found
-        });
+            //Get books list from web api
+            api.getComments(bookId).then(function (response) {
+                $ctrl.comments = response.data;
+            }, function () {
+                //404 not found
+            });
+        }
 
         this.$onInit = function () {
             html.addClass('freeze-scroll');
+            getData();
         };
 
         this.$onDestroy = function () {
             html.removeClass('freeze-scroll');
         };
 
-        $ctrl.submitReview = function() {
-            if ($ctrl.isAnon == 1) {
-                api.submitReview($ctrl.comment, $ctrl.rating, 1, userId, bookId)
+        $ctrl.submitReview = function () {
+            if ($ctrl.isAnon != 1)
+                $ctrl.IsAnon = 0;
+
+            api.submitReview($ctrl.comment, $ctrl.rating, $ctrl.isAnon, userId, bookId)
                 .then(function (response) {
+                    getData();
+                    $ctrl.comment = ""; //reset value
                     alert("Review Submitted!");
-                    location.reload();
                 });
-            } else {
-                api.submitReview($ctrl.comment, $ctrl.rating, 0, userId, bookId)
-                    .then(function (response) {
-                        alert("Review Submitted!");
-                        location.reload();
-                });
-            }
         };
 
-        $ctrl.markPurchased = function() {
+        $ctrl.markPurchased = function () {
 
         };
-
     }
 };
