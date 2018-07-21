fe329fc772bd998a3e878ea8d9f9582ce7f44b3c
Andrew Castillo <andrewcastillo@ADC-MacBook-Air.local>
2018-07-20 23:06:06 -0400

    sort and filters

--- app/books/books.component.js
+++ app/books/books.component.js
@@ -6,8 +6,11 @@
         $ctrl.listing = []; //store books before filter
         $ctrl.search = $stateParams.search;
         $ctrl.genres = [];
+        $ctrl.topSeller = [];
+        $ctrl.sort = {};
         $ctrl.filter = {};
         $ctrl.resultsPerPage = [10, 20, 30, 40, 50];
+        $ctrl.ratings = [0,1,2,3,4,5];
         $ctrl.page = new paginator();
         $ctrl.page.Number = 0;
         $ctrl.page.Size = 10;
@@ -21,21 +24,53 @@
             });
 
             api.getBookGenres().then(function (response) {
+                //response.data has list of genres for dropdown
                 $ctrl.genres = $filter('orderBy')(response.data, 'Genre'); //sort genre dropdown
             });
 
-        }
+            console.log($ctrl.listing.Rating);
 
+            for(b in $ctrl.books){
+                if($ctrl.topSeller < $ctrl.books[b].NumPurchased){
+                    $ctrl.topSeller[0] = $ctrl.books[b];
+                }
+
+            }
+            console.log($ctrl.topSeller);
+                   
+        }
+        
 
         this.updateFilter = function () {
             var result = $ctrl.listing;
-
             //clears empty filters
             for (var prop in $ctrl.filter) {
                 if ($ctrl.filter[prop] == null || $ctrl.filter[prop] == false)
                     $ctrl.filter[prop] = undefined;
             }
+            if($ctrl.sort.isTopSeller == true){
+                result = $filter('orderBy')(result, "-NumPurchased");
+            }
+            if($ctrl.sort.title == true){
+                result = $filter('orderBy')(result, "BookTitle");
+            }
+            if($ctrl.sort.author == true){
+                result = $filter('orderBy')(result, "AuthorName");
+            }
+            if($ctrl.sort.topPrice == true){
+                result = $filter('orderBy')(result, "Price");
+            }
+            if($ctrl.sort.bookRating == true){
+                result = $filter('orderBy')(result, "-Rating");
+            }
+            if($ctrl.sort.releaseDate  == true){
+                result = $filter('orderBy')(result, "-ReleaseDate");
+            }
 
+            console.log($ctrl.listing);
+            console.log($ctrl.isTopSeller);
+            console.log($ctrl.topSeller);
+            console.log($ctrl.filter);
 
 
             //filter results
@@ -50,6 +85,8 @@
             result = $filter('paginator')(result, $ctrl.page);
 
             $ctrl.books = result;
+
+
         }
 
         this.clearFilter = function () {
